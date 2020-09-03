0b79081611420467070165916deb21e241e96cc5
d9nich <dan.galaiko@gmail.com>
2020-09-03 22:36:20 +0300

    Yoho, I`m the king of data.

--- main.py
+++ main.py
@@ -14,32 +14,47 @@ class MyHTMLParser(HTMLParser):
         self.in_td = False
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'table' or self.in_table:
+        if tag == 'table':
             self.in_table = True
-            print("Encountered a start tag:", tag)
+        elif tag == 'tr':
+            self.table.append([])
+            self.in_tr = True
+        elif tag == 'td':
+            self.table[-1].append([])
+            self.in_td = True
 
     def handle_endtag(self, tag):
-        if self.in_table:
-            print("Encountered an end tag :", tag)
         if tag == 'table':
             self.in_table = False
-
+        elif tag == 'tr':
+            self.in_tr = False
+        elif tag == 'td':
+            self.in_td = False
 
     def handle_data(self, data):
-        if self.in_table:
-            print("Encountered some data  :", data)
+        if self.in_td:
+                self.table[-1][-1].append(data)
 
 
 def get_text(url: str) -> str:
     text = urlopen(url).read().decode()
     return text
 
+def print_table(table):
+    for row in table:
+        for data in row:
+            for information in data:
+                print(information, end=' | ')
+            print()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     text = get_text(URL)
     parser = MyHTMLParser()
     parser.feed(text)
+    table = parser.table
     parser.close()
+    print_table(table)
+    print(table)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
