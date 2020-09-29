fab508774e8fdaf5eaadbcb2dc95555e0911b714
d9nich <dan.galaiko@gmail.com>
2020-09-29 22:59:55 +0300

    Some style to my time table has been added.

--- main.py
+++ main.py
@@ -42,9 +42,10 @@ def get_text(url: str) -> str:
     return text
 
 
-def print_table(table):
+def print_table(table, title):
     with open(NAME_OF_OUTPUT_FILE, 'a') as f:
-        print('<table>', file=f)
+        print('<h2 class="text-center">{}</h2>'.format(title), file=f)
+        print('<table class="table table-stripped table-bordered table-hover">', file=f)
         for row in table:
             print('<tr>', file=f)
             for data in row:
@@ -71,13 +72,23 @@ def writeHeadOfHTML():
 <head>
     <meta charset="UTF-8">
     <title>MyTimeTable</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>''', file=f)
 
 
 def writeTailOfHTML():
     with open(NAME_OF_OUTPUT_FILE, 'a') as f:
-        print('''</body></html>''', file=f)
+        print('''<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script></body></html>''', file=f)
 
 
 # Press the green button in the gutter to run the script.
@@ -92,6 +103,6 @@ if __name__ == '__main__':
     # print_table(first)
     # print(first)
     writeHeadOfHTML()
-    print_table(first)
-    print_table(second)
+    print_table(first, 'Перший тиждеь')
+    print_table(second, 'Другий тиждень')
     writeTailOfHTML()
