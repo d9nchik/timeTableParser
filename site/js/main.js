6e38b6752956fe58345d2da694193dffcaec6d36
d9nich <dan.galaiko@gmail.com>
2020-09-30 14:41:25 +0300

    I'm glad to present my time table.

--- site/js/main.js
+++ site/js/main.js
@@ -12,11 +12,45 @@ function getWeekNumber(d) {
     // Get first day of year
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
     // Return array of year and week number
-    return weekNo;
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
 }
 
 let date = new Date();
+let weekNumber = getWeekNumber(date) % 2 + 1
+selectDayOfWeek(weekNumber, date.getDay());
 
-selectDayOfWeek(getWeekNumber(date) % 2 + 1, date.getDay());
+function selectPair(weekNumber, dayNumber, pairNumber) {
+    while (true) {
+        let node =
+            document.querySelector("table:nth-child(" + weekNumber * 2 + ") tbody tr:nth-child(" + pairNumber
+                + ") td:nth-child(" + (dayNumber + 1) + ")");
+        //node can be null in case if getNumbersOfPair return size + 1 value
+        if (node && node.innerText) {
+            node.setAttribute('class', 'table-warning');
+            break;
+        }
+        pairNumber++;
+        if (pairNumber > 5) {
+            pairNumber = 1;
+            dayNumber++;
+            if (dayNumber > 5) {
+                dayNumber = 1;
+                weekNumber = weekNumber % 2 + 1;
+            }
+        }
+    }
+}
+
+
+selectPair(weekNumber, date.getDay(), getNumberOfPair(date));
+
+function getNumberOfPair(date) {
+    const endOfPair = [830, 1200, 1355, 1550, 1745];
+    let time = date.getHours() * 100 + date.getMinutes();
+    for (let i = 0; i < endOfPair.length; i++) {
+        if (time < endOfPair[i])
+            return i + 1;
+    }
+    return endOfPair.length + 1;
+}
