16feb6a318ac58f9f4f0f515129d676fbe27218d
d9nich <dan.galaiko@gmail.com>
2020-12-15 13:12:29 +0200

    deleted unused js

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def print_table(table, title):
             print('</th>', file=f)
         print('</tr>', file=f)
         print(file=f)
-        table=table[1:]
+        table = table[1:]
         print('</thead><tbody>', file=f)
         for row in table:
             print('<tr>', file=f)
@@ -94,16 +94,7 @@ def writeHeadOfHTML():
 
 def writeTailOfHTML():
     with open(NAME_OF_OUTPUT_FILE, 'a') as f:
-        print('''<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-        crossorigin="anonymous"></script>
-<script src="js/main.js"></script></body></html>''', file=f)
+        print('''<script src="js/main.js"></script></body></html>''', file=f)
 
 
 # Press the green button in the gutter to run the script.
